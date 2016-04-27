baf80aa5d48f3977df1baf4e2de9ebe36e49e6f6
nivek29 <kevinmezamc@gmail.com>
2016-04-27 08:50:14 -0600

    7

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
 
@@ -18,7 +19,7 @@ def post_detail(request, pk):
 
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
+@login_required
 def post_new(request):
 
     if request.method == "POST":
@@ -41,6 +42,7 @@ def post_new(request):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
@@ -65,12 +67,14 @@ def post_edit(request, pk):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
 
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
@@ -79,6 +83,7 @@ def post_publish(request, pk):
 
     return redirect('blog.views.post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
